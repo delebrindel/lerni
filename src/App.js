b8b5b806410c60952a7bb8ec6b897316509ec304
Cristóbal Torres <delebrindel@gmail.com>
2019-05-08 15:24:18 -0500

    State first class

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Person from "./Person/Person"
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Kaito", age: 15},
+      {name: "Tsuki", age: 18},
+      {name: "Angelito", age: 4}
+    ]
+  }
   render(){
     return (
       <div className="App">
-      <Person className="person" name="Kaito" age="15" />
+      <Person className="person" name={this.state.persons[0].name} age={this.state.persons[0].age} />
       <Person className="person" name="Tsuki" age="18">She is not here though.</Person>
       <Person className="person" name="Angelito" age="4" />
     </div>
