9b4712b2313f4474c6d5db57cfef56936dbe7701
Cristóbal Torres <delebrindel@gmail.com>
2019-05-09 13:52:39 -0500

    State management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from "./Person/Person"
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState] = useState({
     persons: [
       {name: "Kaito", age: 15},
       {name: "Tsuki", age: 18},
       {name: "Angelito", age: 4}
     ]
+  });
+  const switchNameHandler = () =>{
+    setPersonsState({
+      persons: [
+        {name: "Kaitongo", age: 15},
+        {name: "Tsukina", age: 18},
+        {name: "Angelillo", age: 4}
+      ]
+    })
   }
-  render(){
-    return (
-      <div className="App">
-      <Person className="person" name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person className="person" name="Tsuki" age="18">She is not here though.</Person>
-      <Person className="person" name="Angelito" age="4" />
+  return (
+    <div className="App">
+    <h1>Hi I am a React app</h1>
+    <button onClick={switchNameHandler}>Click Me</button>
+    <Person className="person" name={personsState.persons[0].name} age={personsState.persons[0].age} />
+    <Person className="person" name={personsState.persons[1].name} age={personsState.persons[1].age}>She is not here though.</Person>
+    <Person className="person" name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
-  );
-}
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (props) =>{
     return(
-        <div>
+        <div className="person">
             <h1>I'm {props.name}!</h1>
             <p>My age is {props.age}</p>
             <p>{props.children}</p>
